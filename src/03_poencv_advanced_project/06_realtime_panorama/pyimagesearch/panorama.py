6760c2355b464c94c1f0dd014a41d9b85048fdd5
zzingotri <zzingo@trineedle.com>
2021-01-12 08:48:02 +0900

    realtime_stitching.py

--- src/03_poencv_advanced_project/06_realtime_panorama/pyimagesearch/panorama.py
+++ src/03_poencv_advanced_project/06_realtime_panorama/pyimagesearch/panorama.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+# import the necessary packages
 import numpy as np
 import imutils
 import cv2
@@ -6,37 +7,34 @@ import cv2
 
 class Stitcher:
     def __init__(self):
-        # determine if we are using OpenCV v3.X
-        self.isv3 = imutils.is_cv3(or_better=True)
+        # determine if we are using OpenCV v3.X and initialize the
+        # cached homography matrix
+        self.isv3 = imutils.is_cv3()
+        self.cachedH = None
 
-    def stitch(self, images, ratio=0.75, reprojThresh=4.0,
-               showMatches=False):
-        # unpack the images, then detect keypoints and extract
-        # local invariant descriptors from them
+    def stitch(self, images, ratio=0.75, reprojThresh=4.0):
+        # unpack the images
         (imageB, imageA) = images
-        (kpsA, featuresA) = self.detectAndDescribe(imageA)
-        (kpsB, featuresB) = self.detectAndDescribe(imageB)
-        # match features between the two images
-        M = self.matchKeypoints(kpsA, kpsB,
-                                featuresA, featuresB, ratio, reprojThresh)
-        # if the match is None, then there aren't enough matched
-        # keypoints to create a panorama
-        if M is None:
-            return None
-
-        # otherwise, apply a perspective warp to stitch the images
-        # together
-        (matches, H, status) = M
-        result = cv2.warpPerspective(imageA, H,
+        # if the cached homography matrix is None, then we need to
+        # apply keypoint matching to construct it
+        if self.cachedH is None:
+            # detect keypoints and extract
+            (kpsA, featuresA) = self.detectAndDescribe(imageA)
+            (kpsB, featuresB) = self.detectAndDescribe(imageB)
+            # match features between the two images
+            M = self.matchKeypoints(kpsA, kpsB,
+                                    featuresA, featuresB, ratio, reprojThresh)
+            # if the match is None, then there aren't enough matched
+            # keypoints to create a panorama
+            if M is None:
+                return None
+            # cache the homography matrix
+            self.cachedH = M[1]
+        # apply a perspective transform to stitch the images together
+        # using the cached homography matrix
+        result = cv2.warpPerspective(imageA, self.cachedH,
                                      (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
         result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
-        # check to see if the keypoint matches should be visualized
-        if showMatches:
-            vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches,
-                                   status)
-            # return a tuple of the stitched image and the
-            # visualization
-            return (result, vis)
         # return the stitched image
         return result
 
