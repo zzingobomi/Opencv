0474ea1c35fecd67216226dc95be50f6b8818104
zzingotri <zzingo@trineedle.com>
2021-01-29 16:43:07 +0900

    02 data augmentation

---
+++ src/03_practitioner_bundle/02_data_augmentation/augmentation_demo.py
@@ -0,0 +1,7 @@
+# import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+import numpy as np
+import argparse
+
